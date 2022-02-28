c4a87b7392e21a3c0de4ddeb70896429cd67ae4e
Cameron Stepanski <100606318+camtstep@users.noreply.github.com>
2022-02-28 11:31:06 -0800

    Create dictionary.h

---
+++ dictionary.h
@@ -0,0 +1,17 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+#include <string>
+
+using namespace std;
+
+int MinWordLength(string words[], int length);
+
+int MaxWordLength(string words[], int length);
+
+int WordLengthRange(string words[], int length);
+
+int AverageWordLength(string words[], int length);
+
+int MostCommonWordLength(string words[], int length);
+
+#endif
